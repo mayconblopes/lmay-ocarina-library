52fd13227df8f6ffb77bb28b9ccc0d8c76ad36ea
Maycon Lopes <mayconblopes@gmail.com>
2026-08-17 11:21:03 -0300

    chore: initialize Astro project

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,10 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 export default defineConfig({
+  //O site informa ao Astro qual será o endereço público.
+  site: "https://mayconblopes.github.io",
+
+  //O base informa que a aplicação não estará na raiz do domínio
+  base: "/lmay-ocarina-library",
   integrations: [react()],
 });
